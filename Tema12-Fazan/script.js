f10ecda0b4ad3676fd5a87c5e86c4f33bfead632
eric <ericpredit@gmail.com>
2025-05-13 00:01:44 +0300

    changing letters

--- Tema12-Fazan/script.js
+++ Tema12-Fazan/script.js
@@ -1,56 +1,55 @@
-let startColor = { r: 120, g: 166, b: 90 }
-let stopColor = { r: 187, g: 39, b: 26 }
-let letterSwapping = false
-let intervalID
-let asciiCodeStart = 65
-let asciiCode = asciiCodeStart
+let startColor = { r: 120, g: 166, b: 90 };
+let stopColor = { r: 187, g: 39, b: 26 };
+let letterSwapping = false;
+let intervalID;
+let asciiCodeStart = 65;
+let asciiCode = asciiCodeStart;
+let currentLetter = 'A';
 
 function setup() {
-    createCanvas(700, 500)
-
+    createCanvas(700, 300);
+    textAlign(CENTER, CENTER);
 }
-
 
 function draw() {
+    background("white");
 
-    background("white")
+    // Draw buttons
+    button(300, 200, 100, 40, startColor, "START");
+    button(500, 200, 100, 40, stopColor, "STOP");
 
-    button(300, 100, 100, 40, startColor, "start")
-    button(500, 100, 100, 40, stopColor, "stop")
+    // Display current letter in black
+    fill(0);
+    textSize(80);
+    text(currentLetter, width / 2, height / 2 - 50);
 }
 
-
 function button(x, y, w, h, color, textContent) {
-
-    fill(color.r, color.g, color.b)
-    rect(x, y, w, h, 7)
-    fill("black")
-    textSize(24)
-    text(textContent, x + 27, y + 27)
+    fill(color.r, color.g, color.b);
+    rect(x, y, w, h, 7);
+    fill("white");
+    textSize(20);
+    text(textContent, x + w / 2, y + h / 2);
 }
 
 function mouseClicked() {
-    if (mouseX > 300 && mouseX < 400 && mouseY > 100 && mouseY < 140) {
-        console.log("start")
-        if (!colorSwapping)
-            intervalID = setInterval(changeButtonColor, 1000)
-        letterSwapping = true;
+    // START button
+    if (mouseX > 300 && mouseX < 400 && mouseY > 200 && mouseY < 240) {
+        if (!letterSwapping) {
+            intervalID = setInterval(changeLetter, 500);
+            letterSwapping = true;
+        }
     }
-    if (mouseX > 500 && mouseX < 600 && mouseY > 100 && mouseY < 140) {
-        console.log("stop")
-        clearInterval(intervalID)
+
+    // STOP button
+    if (mouseX > 500 && mouseX < 600 && mouseY > 200 && mouseY < 240) {
+        clearInterval(intervalID);
         letterSwapping = false;
     }
 }
 
-
 function changeLetter() {
-    asciiCode++
-    console.log(String.fromCharCode(65, 66, 67))
-}
-
-function showLetter(letter) {
-    fill("black")
-    textSize(30)
-    text(letter, 100, 100)
+    asciiCode++;
+    if (asciiCode > 90) asciiCode = asciiCodeStart;
+    currentLetter = String.fromCharCode(asciiCode);
 }
