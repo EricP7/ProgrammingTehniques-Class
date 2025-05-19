602151a162c5c6494ca3a65676c3ea0a4de71b9d
eric <ericpredit@gmail.com>
2025-05-19 23:26:44 +0300

    pawns created

--- Tema13-ChessTable/script.js
+++ Tema13-ChessTable/script.js
@@ -1,10 +1,12 @@
 const chessBoxes = []
+const pawns = []
 
 function setup() {
 
     createCanvas(1000, 1000);
 
     initBoxes()
+    initPawns()
 }
 
 function draw() {
@@ -12,6 +14,7 @@ function draw() {
     background("gray")
 
     drawTable()
+    drawPawns()
 }
 
 function drawTable() {
@@ -54,3 +57,31 @@ function initBoxes() {
     }
 }
 
+function initPawns() {
+    pawns.length = 0
+    const used = new Set()
+    const colCount = Array(8).fill(0) // Track pawns per column
+
+    while (pawns.length < 16) {
+        const row = Math.floor(Math.random() * 8)
+        const col = Math.floor(Math.random() * 8)
+        const key = row + ',' + col
+
+        // Only add if not used and column has less than 2 pawns
+        if (!used.has(key) && colCount[col] < 2) {
+            used.add(key)
+            // Assign color based on row
+            const color = row < 4 ? "black" : "white"
+            pawns.push({ row, col, color })
+            colCount[col]++
+        }
+    }
+}
+
+function drawPawns() {
+    for (const pawn of pawns) {
+        fill(pawn.color)
+        const box = chessBoxes[pawn.row][pawn.col]
+        ellipse(box.x + box.size / 2, box.y + box.size / 2, box.size * 0.6)
+    }
+}
