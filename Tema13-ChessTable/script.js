ed3f3b8f9df2105731feb0fe44deea242968990b
eric <ericpredit@gmail.com>
2025-05-19 23:13:21 +0300

    chess board

--- Tema13-ChessTable/script.js
+++ Tema13-ChessTable/script.js
@@ -3,17 +3,21 @@ const chessBoxes = []
 function setup() {
 
     createCanvas(1000, 1000);
+
+    initBoxes()
 }
 
 function draw() {
 
     background("gray")
+
+    drawTable()
 }
 
 function drawTable() {
-    for (let rows of 8)
-        for (let cols of 8)
-            createBox
+    for (let rows of chessBoxes)
+        for (let box of rows)
+            createBox(box)
 }
 
 function createBox(obj) {
@@ -23,18 +27,30 @@ function createBox(obj) {
 }
 
 function initBoxes() {
-    let x = 25
-    let y = 25
-    for (let i of 8) {
+    let x = 100
+    let y = 100
+
+
+    for (let i = 0; i < 8; i++) {
+
+        let color = (i % 2 == 0) ? "white" : "black"
+
         const lines = []
         chessBoxes.push(lines)
-        for (let j of 8) {
+        for (let j = 0; j < 8; j++) {
             const box = {
                 x: x,
                 y: y,
                 size: 80,
-                color: {}
+                color: color
             }
+            lines.push(box)
+            x += 80
+
+            color = (color === "white") ? "black" : "white"
         }
+        x = 100
+        y += 80
     }
 }
+
